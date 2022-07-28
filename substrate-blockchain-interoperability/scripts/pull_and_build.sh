ca0840bb68d9c7eb1f88fcca82dca920899716cc
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2022-07-28 14:38:11 +0200

    fix pull and build

--- substrate-blockchain-interoperability/scripts/pull_and_build.sh
+++ substrate-blockchain-interoperability/scripts/pull_and_build.sh
@@ -8,11 +8,11 @@ echo -e 'This scripts take a while...maybe grab a coffee ? \x00\xe2\x98\x95'
 # [ -d "./substrate-blockchain-parachain-insurance" ] && echo "Directory ./substrate-blockchain-parachain-insurance exists" ||  git clone git@bitbucket.org:edge-team-leat/substrate-blockchain-parachain-mapfre.git ./substrate-blockchain-parachain-insurance #tmp fix for correct path, will change repo mapfre to insurance later
 # [ -d "./substrate-blockchain-relay-chain" ] && echo "Directory ./substrate-blockchain-relay-chain exists" ||  git clone git@bitbucket.org:edge-team-leat/substrate-blockchain-relay-chain.git
 echo "Build parachains" && \
-cd ./substrate-blockchain-parachain-renault  && \
+cd ./common-parachain-node  && \
 cargo build --release  && \
-cd ../ && \
-cd ./substrate-blockchain-parachain-insurance  && \
-cargo build --release  && \
+# cd ../ && \
+# cd ./substrate-blockchain-parachain-insurance  && \
+# cargo build --release  && \
 cd ../ && \
 echo "Build relay chain" && \
 cd ./substrate-blockchain-relay-chain  && \
