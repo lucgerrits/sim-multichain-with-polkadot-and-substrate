03de94155890df99884880f431fce8b57a5fd297
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2022-08-27 12:58:12 +0200

    add test for report accident renault

--- common-parachain-node/pallets/sim_renault_accident/src/tests.rs
+++ common-parachain-node/pallets/sim_renault_accident/src/tests.rs
@@ -7,18 +7,25 @@ use frame_support::{assert_noop, assert_ok, inherent::Vec, traits::IsType};
 use sha2::{digest::Update, Digest, Sha256};
 #[allow(unused_imports)]
 use sp_runtime::DispatchError;
+#[allow(unused_imports)]
 use sp_std::if_std;
 
 #[test]
 fn it_works_for_default_value() {
 	new_test_ext().execute_with(|| {
-		let vehicle: u64 = 1;
 
+		//
+		//Test report_accident() extrinsic
+		//
+
+		//the vehicle id
+		let vehicle: u64 = 1;
+		//create data "Hello World" and hash it
 		let mut hasher = Sha256::new();
 		sha2::Digest::update(&mut hasher, "Hello World".as_bytes());
 		let data_hash = hasher.finalize().into();
 
-		// create a new accident
+		// create a new accident report
 		assert_ok!(SimRenaultAccidentPallet::report_accident(
 			Origin::signed(vehicle),
 			vehicle,
@@ -27,22 +34,20 @@ fn it_works_for_default_value() {
 
 		// is accident stored
 		assert!(AccidentCount::<Test>::contains_key(vehicle));
-
+		//get the number of reported accidents, should be == 1
 		let count: u32 = match AccidentCount::<Test>::get(vehicle) {
 			// Return an error if the value has not been set.
 			None => 0,
 			Some(val) => val,
 		};
-		if_std! {
-			println!("Accident Count={}", count);
-		}
+		assert!(count == 1);
+
+		//now check the stored report: data is a composit key == hash(vehicle id + (count-1))
 		let mut hasher = Sha256::new();
 		sha2::Digest::update(&mut hasher, vehicle.to_ne_bytes());
 		sha2::Digest::update(&mut hasher, (count - 1).to_ne_bytes()); //-1 because we start at 0
 		let accident_key: [u8; 32] = hasher.finalize().into();
-		if_std! {
-			println!("{:02x?}", accident_key);
-		}
+		//check if contains an accident report
 		assert!(Accidents::<Test>::contains_key(accident_key));
 	});
 }
