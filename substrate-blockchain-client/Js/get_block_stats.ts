9644c1d0ae78035a1e71f1cf5858f98cf029da73
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2022-10-17 17:46:24 +0200

    update get stats script + plot

--- substrate-blockchain-client/Js/get_block_stats.ts
+++ substrate-blockchain-client/Js/get_block_stats.ts
@@ -24,29 +24,30 @@ const myApp = async () => {
     const parachainApiInstInsurance = await parachainApi('ws://127.0.0.1:8843');
     const relaychainApiInst = await relaychainApi('ws://127.0.0.1:9944');
 
+    let rows_blocktime: any[] = []
+    let rows_extrinsic_cnt: any[] = []
+    let last_n_blocks = parseInt(process.argv[2]) || 0 //0
     let csv_separator = ","
     let path_prefix = "../../results/block_logs/"
     if (!fs.existsSync(path_prefix))
         fs.mkdirSync(path_prefix)
     let block_min = -1;
-    for (let api of [parachainApiInstRenault, parachainApiInstInsurance, relaychainApiInst]) {
+    for (let api of [parachainApiInstRenault, parachainApiInstInsurance]) { //relaychainApiInst
         const chain_name = (await api.rpc.system.chain()).toString();
         log("Chain: " + chain_name)
-        let filename_blocktime = path_prefix + "block_times_" + chain_name + ".csv"
-        let filename_extrinsic_cnt = path_prefix + "extrinsic_cnt_" + chain_name + ".csv"
-        if (fs.existsSync(filename_blocktime))
-            fs.unlinkSync(filename_blocktime)
-        if (fs.existsSync(filename_extrinsic_cnt))
-            fs.unlinkSync(filename_extrinsic_cnt)
+        let filename_blockstats = path_prefix + "block_stats_" + chain_name + ".csv"
+        if (fs.existsSync(filename_blockstats))
+            fs.unlinkSync(filename_blockstats)
 
         // make CSV headers
-        fs.appendFileSync(filename_blocktime, "block" + csv_separator + "time" + "\n")
-        fs.appendFileSync(filename_extrinsic_cnt, "block" + csv_separator + "transactions" + "\n")
+        fs.appendFileSync(filename_blockstats, "block" + csv_separator + "timestamp" + csv_separator + "blocktime" + csv_separator + "transactions" + csv_separator + "tps" + "\n")
 
         let current_block_number = await (await api.derive.chain.bestNumberFinalized()).toNumber();
         let current_block_data = await api.derive.chain.getBlockByNumber(0);
-        let block_nb = 0;
+        let block_nb = (current_block_number - last_n_blocks) > 0 ? (current_block_number - last_n_blocks) : 0;
         let previous_time = '0';
+        let saved_a_time = false
+        // log("Current block is #" + current_block_number)
         while (block_nb != current_block_number) {
             // log("#" + block_nb)
             current_block_data = await api.derive.chain.getBlockByNumber(block_nb);
@@ -61,8 +62,10 @@ const myApp = async () => {
                         let current_time = value.args.toString();
                         if (previous_time !== '0') { //can only calculate block time for blocks > 1
                             let diff = parseInt(current_time) - parseInt(previous_time)
-                            let data = block_nb + csv_separator + (diff / 1000).toString() + "\n"
-                            fs.appendFileSync(filename_blocktime, data)
+                            rows_blocktime.push([block_nb, current_time, (diff / 1000), current_block_data?.block?.extrinsics.length, (current_block_data?.block?.extrinsics.length / (diff / 1000))])
+                            // let data = block_nb + csv_separator + (diff / 1000).toString() + "\n"
+                            // fs.appendFileSync(filename_blockstats, data)
+                            saved_a_time = true
                         }
                         previous_time = current_time
                     }
@@ -70,22 +73,26 @@ const myApp = async () => {
                     // log(index)
                     // log(arr.toString())
                 })
-                // store the extrinsic count in file
-                let data = block_nb + csv_separator + current_block_data?.block?.extrinsics.length + "\n"
-                fs.appendFileSync(filename_extrinsic_cnt, data)
             }
             block_nb = block_nb + 1;
             // stop at the same height as smallest parachain block height
-            if (chain_name === "Rococo Local Testnet" && block_nb > block_min)
-                break;
+            // if (chain_name === "Rococo Local Testnet" && block_nb > block_min)
+            //     break;
         }
         // Get the smallest parachain block height
-        if (block_min == -1 && chain_name != "Rococo Local Testnet")
-            block_min = block_nb
-        if (block_min > block_nb && chain_name != "Rococo Local Testnet")
-            block_min = block_nb
-    }
+        // if (block_min == -1 && chain_name != "Rococo Local Testnet")
+        //     block_min = block_nb
+        // if (block_min > block_nb && chain_name != "Rococo Local Testnet")
+        //     block_min = block_nb
 
+
+        let csvContent_blocktime = ""
+        rows_blocktime.forEach(function (rowArray) {
+            let row = rowArray.join(csv_separator);
+            csvContent_blocktime += row + "\n";
+        });
+        fs.appendFileSync(filename_blockstats, csvContent_blocktime)
+    }
     process.exit(0)
 };
 
