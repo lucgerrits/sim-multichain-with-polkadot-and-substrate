46fc85cb0eb2d32dcca432844e55b1870c2dc07e
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2023-01-17 15:01:33 +0100

    add options to add parachain script

--- substrate-blockchain-interoperability/scripts/add_parachains.sh
+++ substrate-blockchain-interoperability/scripts/add_parachains.sh
@@ -27,14 +27,46 @@ if [ -z "$1" ]
     then
         echo "No specific endpoint"
     else
-        relay_chain_endpoint="wss://relaychain.gerrits.xyz"
+        relay_chain_endpoint=$1
 fi
 echo "Using endpoint: $relay_chain_endpoint"
+
+if [ -z "$2" ]
+    then
+        echo "No specific renault state path"
+    else
+        RENAULT_GENESIS_STATE_PATH=$2
+fi
+echo "Using renault state path: $RENAULT_GENESIS_STATE_PATH"
+
+if [ -z "$3" ]
+    then
+        echo "No specific renault wasm path"
+    else
+        RENAULT_RUNTIME_WASM_PATH=$3
+fi
+echo "Using renault wasm path: $RENAULT_RUNTIME_WASM_PATH"
+
+if [ -z "$4" ]
+    then
+        echo "No specific insurance state path"
+    else
+        INSURANCE_GENESIS_STATE_PATH=$4
+fi
+echo "Using insurance state path: $INSURANCE_GENESIS_STATE_PATH"
+
+if [ -z "$5" ]
+    then
+        echo "No specific insurance wasm path"
+    else
+        INSURANCE_RUNTIME_WASM_PATH=$5
+fi
+echo "Using insurance wasm path: $INSURANCE_RUNTIME_WASM_PATH"
 
 cd ../ #to use config vars like other scripts
 
 echo "...waiting that relay chain started"
-sleep 20 #increase if parachains are not started fast enough !
+# sleep 90 #increase if parachains are not started fast enough !
 
 ./scripts/add_parachains.js 2000 $RENAULT_GENESIS_STATE_PATH $RENAULT_RUNTIME_WASM_PATH $relay_chain_endpoint
 ./scripts/add_parachains.js 3000 $INSURANCE_GENESIS_STATE_PATH $INSURANCE_RUNTIME_WASM_PATH $relay_chain_endpoint
