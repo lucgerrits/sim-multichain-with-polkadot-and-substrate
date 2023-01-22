e2536ef06d3eedf14a83662f0386e16cd42c1ee8
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2023-01-22 19:39:08 +0100

    add script to re-add parachains if para lease expired

---
+++ substrate-blockchain-interoperability/scripts/re_add_parachains.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+
+// Import
+var { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+const fs = require("fs");
+
+(async () => {
+    try {
+        const id = process.argv[2];
+        const url = process.argv[3];
+
+        // Construct
+        const wsProvider = new WsProvider(url);
+        const api = await ApiPromise.create({ provider: wsProvider });
+        const keyring = new Keyring({ type: 'sr25519' });
+
+        const sudoPair = keyring.addFromUri('//Alice');
+
+        // Send the actual sudo transaction
+        const unsub = await api.tx.sudo
+            .sudo(
+                api.tx.parasSudoWrapper.sudoScheduleParathreadUpgrade(parseInt(id))
+            )
+            .signAndSend(sudoPair, (result) => {
+                console.log(`Current status is ${result.status}`);
+
+                if (result.status.isInBlock) {
+                    console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+                } else if (result.status.isFinalized) {
+                    console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
+                    process.exit(0)
+                    unsub();
+                }
+            });
+        // process.exit(0)
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
+})()
