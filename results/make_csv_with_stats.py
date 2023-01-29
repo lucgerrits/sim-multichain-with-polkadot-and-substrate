55f7cb99b8ab4059dc0482cb20d771eea645328c
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2023-01-29 19:57:01 +0100

    test 1 collator finished

--- results/make_csv_with_stats.py
+++ results/make_csv_with_stats.py
@@ -14,7 +14,8 @@ def extract_int_tps(string):
 
 def generate_csv(prefix_path, test_name):
     # Get a list of all CSV files that start with "big_tests_" in the prefix path folder
-    csv_files = glob.glob(f"./block_logs/{prefix_path}/big_tests_{test_name}_*.csv")
+    csv_files = glob.glob(
+        f"./block_logs/{prefix_path}/big_tests_{test_name}_*.csv")
 
     # Create an empty list to store the results
     results = []
@@ -26,6 +27,10 @@ def generate_csv(prefix_path, test_name):
             # Create a CSV reader object
             reader = csv.DictReader(f)
             # Initialize a variable to store the maximum "tps" value
+            total_tx = 30000 if (extract_int_tps(file)*60*2) > 30000 else (extract_int_tps(file)*60*2)
+            percentage_failed_tx = 0
+            success_tx = 0
+            failed_tx = 0
             max_tps = 0
             avg_tps = 0.0
             nb_blocks_used = 0
@@ -39,6 +44,7 @@ def generate_csv(prefix_path, test_name):
                 if float(row['tps']) > max_tps:
                     max_tps = round(float(row['tps']), 2)
                 if int(row['transactions']) > 2:
+                    success_tx += int(row['transactions']) - 2
                     avg_tps += float(row['tps'])
                     nb_blocks_used += 1
                     avg_blocktime += float(row['blocktime'])
@@ -46,6 +52,8 @@ def generate_csv(prefix_path, test_name):
                     blocktime_list.append(float(row['blocktime']))
                 if float(row['blocktime']) > max_blocktime:
                     max_blocktime = round(float(row['blocktime']), 2)
+            percentage_failed_tx = "{}%".format(round((1 - (success_tx/total_tx))*100, 2)) if round((1 - (success_tx/total_tx))*100, 2) > 0.1 else ""
+            failed_tx = total_tx - success_tx
             avg_tps = round(avg_tps/nb_blocks_used, 2)
             avg_blocktime = round(avg_blocktime/nb_blocks_used, 2)
             # Append the current file's name TPS and maximum "tps" value to the results list
@@ -54,14 +62,14 @@ def generate_csv(prefix_path, test_name):
             blocktime_var = round(stats.pvariance(blocktime_list), 2)
             blocktime_std = round(stats.pstdev(blocktime_list), 2)
             results.append([extract_int_tps(file), max_tps, avg_tps, max_blocktime,
-                           avg_blocktime, tps_var, tps_std, blocktime_var, blocktime_std])
+                           avg_blocktime, tps_var, tps_std, blocktime_var, blocktime_std, success_tx, failed_tx, percentage_failed_tx])
 
     # Write the results to a new CSV file
     with open(f'{prefix_path}_{test_name}_stats_values.csv', 'w', newline='') as f:
         results.sort(key=lambda x: x[0])
         writer = csv.writer(f)
         # writer.writerow(['Input TPS', 'Max Output TPS', 'Avg Output TPS', 'Max Block Time', 'Avg Block Time',
-        #                 'TPS Variance', 'TPS Standard Deviation', 'Block Time Variance', 'Block Time Standard Deviation'])
+        #                 'TPS Variance', 'TPS Standard Deviation', 'Block Time Variance', 'Block Time Standard Deviation', 'Success TX', 'Failed TX', 'Percentage Failed TX'])
         writer.writerows(results)
 
 
--- results/stats_cloud_tests.gnuplot
+++ results/stats_cloud_tests.gnuplot
@@ -1,4 +1,4 @@
-set terminal postscript eps enhanced color font 'Times-Roman,18' size 6,5
+set terminal postscript eps enhanced color font 'Times-Roman,20' size 6,6
 set output "stats_cloud_2_collator_tests.eps"
 
 set multiplot layout 2,1 
@@ -24,7 +24,9 @@ set xlabel "Input TPS"
 set datafile separator comma
 
 plot 'renault_2_collator_stats_values.csv' using 3:7:xtic(1) title "Renault avg Output TPS" lc rgbcolor "blue" lt 1, \
-    'insurance_2_collator_stats_values.csv' using 3:7:xtic(1) title "Insurance avg Output TPS" lc rgbcolor "pink" lt 1
+    '' using 0:0:xtic(1):12 with labels font ",15" offset -1,0 tc rgb "red" rotate left notitle, \
+    'insurance_2_collator_stats_values.csv' using 3:7:xtic(1) title "Insurance avg Output TPS" lc rgbcolor "pink" lt 1, \
+    '' using 0:0:12:xtic(1) with labels font ",15" offset 1,0 tc rgb "red" rotate left notitle
 
 
 
@@ -58,7 +60,7 @@ unset multiplot
 ##########################################################################################
 
 
-set terminal postscript eps enhanced color font 'Times-Roman,18' size 6,5
+set terminal postscript eps enhanced color font 'Times-Roman,18' size 6,6
 set output "stats_cloud_1_collator_tests.eps"
 
 set multiplot layout 2,1 
@@ -84,7 +86,9 @@ set xlabel "Input TPS"
 set datafile separator comma
 
 plot 'renault_1_collator_stats_values.csv' using 3:7:xtic(1) title "Renault avg Output TPS" lc rgbcolor "blue" lt 1, \
-    'insurance_1_collator_stats_values.csv' using 3:7:xtic(1) title "Insurance avg Output TPS" lc rgbcolor "pink" lt 1
+    '' using 0:0:xtic(1):12 with labels font ",15" offset -1,0 tc rgb "red" rotate left notitle, \
+    'insurance_1_collator_stats_values.csv' using 3:7:xtic(1) title "Insurance avg Output TPS" lc rgbcolor "pink" lt 1, \
+    '' using 0:0:12:xtic(1) with labels font ",15" offset 1,0 tc rgb "red" rotate left notitle
 
 
 
